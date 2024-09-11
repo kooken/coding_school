9f3c1d646c07654ee7fc9d8ba814c4e198474874
mariasazhina <msazhina23@gmail.com>
2024-09-11 23:54:07 +0300

    first commit

---
+++ JavaScript_Lessons/comments.js
@@ -0,0 +1,13 @@
+// One-line comment - line-ending comments
+
+// Комментарий, который умещается в одну строчку
+
+/*
+Multiple lines comment -
+block comments
+*/
+
+/*
+Комментарий который занимает
+несколько строчек
+*/
